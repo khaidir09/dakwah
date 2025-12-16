c29d60fea78540d541694664ff1415e48b96cb11
Muhammad Khaidir <khaedir3018@gmail.com>
2025-12-16 11:25:04 +0800

    Update edit.blade.php

--- resources/views/pages/jadwal-majelis/edit.blade.php
+++ resources/views/pages/jadwal-majelis/edit.blade.php
@@ -57,6 +57,19 @@
                         </div>
 
                         <div>
+                            <label class="block text-sm font-medium mb-2" for="teacher_id">Nama Guru <span class="text-red-500">*</span></label>
+                            <select id="teacher_id" class="form-select w-full @error('teacher_id') is-invalid @enderror" name="teacher_id" required>
+                                <option value="">Pilih Guru</option>
+                                @foreach($teachers as $item)
+                                    <option value="{{ $item->id }}" @if($item->id == $majelis->teacher_id) selected @endif>{{ $item->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('teacher_id')
+                                <div class="text-xs mt-1 text-red-500">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div>
                             <label class="block text-sm font-medium mb-2" for="waktu">Hari <span class="text-red-500">*</span></label>
                             <select id="hari" class="form-select w-full @error('hari') is-invalid @enderror" name="hari" required>
                                 <option value="{{ old('hari', $jadwal->hari) }}">{{ $jadwal->hari }}</option>
