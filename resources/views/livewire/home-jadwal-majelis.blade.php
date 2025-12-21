6a372b5cadea1ec4b8cac6fab217e9b751c9cdd9
Muhammad Khaidir <khaedir3018@gmail.com>
2025-12-21 21:45:42 +0800

    add tanda akses jamaah

--- resources/views/livewire/home-jadwal-majelis.blade.php
+++ resources/views/livewire/home-jadwal-majelis.blade.php
@@ -10,7 +10,7 @@
                                 <!-- Image + name -->
                                 <header>                
                                     <div class="flex mb-2">
-                                        <a class="relative inline-flex items-start mr-5" href="#0">
+                                        <a class="relative inline-flex items-start mr-5" href="{{ route('guru-detail', $schedule->teacher->id) }}">
                                             @if($schedule->teacher->foto != null)
                                                 <img class="rounded-full w-16 h-16 object-cover" src="{{ Storage::url($schedule->teacher->foto) }}" alt="{{ $schedule->teacher->name }}" />
                                             @else
@@ -28,7 +28,20 @@
                                             <div>{{ $schedule->nama_jadwal }}</div>
                                         </div>
                                     </div>
-                                </header>          
+                                </header>
+                                <div class="relative inline-flex shrink-0">
+                                    @php
+                                        $accessColor = match($schedule->access) {
+                                            'Umum' => 'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300',
+                                            'Ikhwan' => 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300',
+                                            'Akhwat' => 'bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300',
+                                            default => 'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300',
+                                        };
+                                    @endphp
+                                    <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium {{ $accessColor }}">
+                                        {{ $schedule->access }}
+                                    </span>
+                                </div>             
                             </div>
                             <!-- Bio -->
                             <div class="mt-2">
