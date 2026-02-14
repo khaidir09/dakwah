fe9174197bf468a4918bb137d1291a3ab415647e
Muhammad Khaidir <khaedir3018@gmail.com>
2026-02-14 22:43:25 +0800

    Update dashboard.blade.php

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -15,13 +15,13 @@
         <div class="grid grid-cols-12 gap-6">
 
             <!-- Line chart (Acme Plus) -->
-            <x-dashboard.dashboard-card-01 :dataFeed="$dataFeed" />
+            {{-- <x-dashboard.dashboard-card-01 :dataFeed="$dataFeed" /> --}}
 
             <!-- Line chart (Acme Advanced) -->
-            <x-dashboard.dashboard-card-02 :dataFeed="$dataFeed" />
+            {{-- <x-dashboard.dashboard-card-02 :dataFeed="$dataFeed" /> --}}
 
             <!-- Line chart (Acme Professional) -->
-            <x-dashboard.dashboard-card-03 :dataFeed="$dataFeed" />
+            {{-- <x-dashboard.dashboard-card-03 :dataFeed="$dataFeed" /> --}}
 
             <!-- Bar chart (Direct vs Indirect) -->
             <x-dashboard.dashboard-card-04 />
